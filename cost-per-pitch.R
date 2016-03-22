756cee9ece9c7d08a96ba5ac9fde0e3da31b5ee7
Btibert3 <btibert3@gmail.com>
2016-03-22 18:19:07 -0400

    added crawl for pitch stats

--- cost-per-pitch.R
+++ cost-per-pitch.R
@@ -19,18 +19,18 @@ SPORT = "baseball"
 LEAGUE = "mlb"
 
 ## get the teams
-teams_raw <- ss_get_result(sport=SPORT, league=LEAGUE, ep = "teams")
+teams_raw <- ss_get_result(sport=SPORT, league=LEAGUE, ep = "teams", walk=TRUE)
 teams <- parse_stattle(teams_raw, "teams")
 
 ## testing: get the picthers from the redsox
-sox_raw <- ss_get_result(sport=SPORT, league=LEAGUE, ep="players", query = list(team_id = "mlb-bos"), walk=TRUE)
-sox_players <-  parse_stattle(sox_raw, "players") 
-with(sox_players, table(position_abbreviation))
-sox_pitchers <- filter(sox_players, position_abbreviation %in% c('RP','SP') & salary > 0)
+# sox_raw <- ss_get_result(sport=SPORT, league=LEAGUE, ep="players", query = list(team_id = "mlb-bos"), walk=TRUE)
+# sox_players <-  parse_stattle(sox_raw, "players") 
+# with(sox_players, table(position_abbreviation))
+# sox_pitchers <- filter(sox_players, position_abbreviation %in% c('RP','SP') & salary > 0)
 
 
 ## loop through the teams get the pitchers with salalry
-pitchers = data.frame()
+pitchers <- data.frame()
 for (TEAM in teams$slug) {
   ## get the players
   tmp_raw <- ss_get_result(sport=SPORT, 
@@ -50,7 +50,26 @@ for (TEAM in teams$slug) {
 }
 
 ## for each pitcher, get their total pitches thrown
+p_stats <- data.frame()
+for (PLAYER in pitchers$slug) {
+  ## get the player total stats
+  tmp_raw <- ss_get_result(sport=SPORT, 
+                           league=LEAGUE, 
+                           ep="total_stats", 
+                           query = list(player_id = PLAYER,
+                                        interval_type = "regularseason",
+                                        type = "baseball_pitcher_stat",
+                                        stat = "pitches_thrown"))
+  ## parse out the data
+  tmp_stat <- tmp_raw[[1]]$total_player_stat$total
+  ## make a data frame
+  tmp_stat <- data.frame(slug = PLAYER, pitches = tmp_stat)
+  ## bind to the stats
+  p_stats = bind_rows(p_stats, tmp_stat)
+  rm(tmp_raw, tmp_stat)
+  cat("finished ", PLAYER, "\n")
+}
 
 
-
+## 
 
